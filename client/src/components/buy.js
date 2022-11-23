63e64f51b1d7792b547535eb17318cfaa906ec17
C20432946 <c20432946@mydit.ie>
2022-11-23 16:32:43 +0000

    Functioning ads from databse

--- client/src/components/buy.js
+++ client/src/components/buy.js
@@ -1,14 +1,46 @@
 import React from "react";
-import { Cars } from "./cars.js";
+import { carData } from "./carModels";
+import "./buy.css";
 
+export const Buy = () => {
+  return (
+    <div classname = "carContainer">
+        {carData.map((data, key) => {
+          return ( 
+            <div key={key}>
+                <Carad
+                key={key}
+                name = {data.name} 
+                Engine = {data.Engine}                 
+                Reg = {data.Reg} 
+                Colour = {data.Colour}
+                Price = {data.Price}
+                Image = {data.Image} />
+            </div>
+          );
+        })}
+      </div>
+  );
+};
 
-export default function Buy() {
-      
+export default Buy;
+
+const Carad = ({ name, Engine, Reg, Colour, Price, Image }) => {
+    if (!name) return <div />;
     return (
-        <div>
-            <Cars />
-        </div>
-        
-    )
-}
+    
+    <div class = "ad">
+        <img src = {Image} alt=''></img>
+        <h4> {name}</h4>
+        <hr></hr>
+        <h5>{Engine} litre</h5>
+        <h5>{Price}</h5>
+        <h5>{Colour}</h5>
+        <button><i className="fa fa-heart"></i></button>
+    </div>
+    
+    );
+};
+
+
 
--- client/src/components/home.js
+++ client/src/components/home.js
@@ -1,54 +1,15 @@
 import React from "react";
 import './home.css';
+import { carData } from "./carModels";
 
-export default function Home() {
-    return (
-        <div class = "homemaindiv">
-            <h2>Featured Ads</h2>
-        <div class = "featureddiv">
+export const Home = () => {
+  return (
+    <div classname = "homecontainer">
+        <h2>Homepage</h2>
+      </div>
+  );
+};
 
-                <div class = "ad">
-                <img src = "audi.jpg"alt=''></img>
-                <h4>Audi A4</h4>
-                <hr></hr>
-                <h5>€2,000</h5>
-                <h5>Laois</h5>
-                <button>See more</button>
-                <button><i className="fa fa-heart"></i></button>
-                </div>
-
-                <div class = "ad">
-                    <img src = "bmw.jpg" alt=''></img>
-                    <h4> BMW</h4>
-                    <hr></hr>
-                    <h5>€6,000</h5>
-                    <h5>Kildare</h5>
-                    <button>See more</button>
-                    <button><i className="fa fa-heart"></i></button>
-                </div>
-
-                <div class = "ad">
-                    <img src = "skoda.jpg"alt=''></img>
-                    <h4> Skoda</h4>
-                    <hr></hr>
-                    <h5>€8,000</h5>
-                    <h5>Dublin</h5>
-                    <button>See more</button>
-                    <button><i className="fa fa-heart"></i></button>
-                </div>
-
-                <div class = "ad">
-                    <img src = "ford.jpeg"alt=''></img>
-                    <h4> Ford</h4>
-                    <hr></hr>
-                    <h5>€10,000</h5>
-                    <h5>Meath</h5>
-                    <button>See more</button>
-                    <button><i className="fa fa-heart"></i></button>
-                </div>
-            </div>
-        </div>
-        );
-    }
+export default Home;
 
 
