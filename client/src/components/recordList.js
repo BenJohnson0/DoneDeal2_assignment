4385f5289af6e841815e1c2695d5a344b7272ecc
C20432946 <c20432946@mydit.ie>
2022-11-25 12:33:51 +0000

    fixed responsiveness, account edit changes

--- client/src/components/recordList.js
+++ client/src/components/recordList.js
@@ -9,7 +9,7 @@ const Record = (props) => (
    <td>{props.record.password}</td>
    <td>
      <Link className="btn btn-link" to={`/edit/${props.record._id}`}>Edit</Link> |
-     <button className="btn btn-link"
+     <button id ="deletebutton" className="btn btn-link"
        onClick={() => {
          props.deleteRecord(props.record._id);
        }}
