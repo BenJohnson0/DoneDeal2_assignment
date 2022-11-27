b9e04866b7cb61df7c48a07d5fbc7201f38356e3
C20432946 <c20432946@mydit.ie>
2022-11-27 13:28:07 +0000

    more responsive changes

--- client/src/components/help.js
+++ client/src/components/help.js
@@ -25,10 +25,9 @@ export default function Help() {
 
                 <div className="contactusdiv">
                 <h3>Find us</h3>
-                    <div class = "googlemaps">
+                 
                         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2381.180775147405!2d-6.2894665841611275!3d53.357919579981456!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48670c02cb588d75%3A0xd288d4099c509cd6!2sTechnological%20University%20Dublin!5e0!3m2!1sen!2sie!4v1669068505156!5m2!1sen!2sie" allowfullscreen="false" loading="lazy" referrerpolicy="no-referrer-when-downgrade" title="maps"></iframe>
             
-                    </div>
                 </div>
 
                 <div className="contactusdiv">
